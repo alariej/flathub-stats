6f88d91cf188d500de50e9c4589d0be9151a5136
alariej <alariej@users.noreply.github.com>
2022-07-12 13:41:52 +0000

    deploy: f145f2aa1e128eca75d412363539a796ef759c21

---
+++ pull_stats.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import os
+import subprocess
+import json
+import shutil
+
+if subprocess.call(["wget", "--execute", "robots=off", "--recursive", "--no-host-directories", "--no-parent", "--reject-regex", "backup", "--accept", "*.json", "https://flathub.org/stats/"]) != 0:
+    exit(1)
+
+files = []
+refs = {}
+
+for (dirpath, _, filenames) in os.walk("stats"):
+    files.extend(os.path.join(dirpath, filename) for filename in filenames)
+files.sort()
+# The latest stats aren't very accurate, please see: https://github.com/klausenbusk/flathub-stats/issues/5
+del files[-1]
+
+for f in files:
+    print(f)
+    with open(f) as json_file:
+        data = json.load(json_file)
+        date = data["date"]
+        for ref in data["refs"]:
+            refs.setdefault(ref, {"ref": ref, "stats": []})["stats"].append({"date": date, "arches": data["refs"][ref]})
+
+if os.path.isdir("data"):
+    shutil.rmtree("data")
+os.mkdir("data")
+
+def writeJson(f, data):
+    with open(f, "w") as outfile:
+        json.dump(data, outfile)
+
+for ref in refs:
+    writeJson(f"data/{ref.replace('/', '_')}.json", refs[ref])
+
+writeJson("data/refs.json", list(refs.keys()))
